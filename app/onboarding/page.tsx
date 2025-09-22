d8c68c062e45f1ef02c93b420582c27309225fa4
miheal-soft-web <micheal.ogbechie@eceesoftware.com>
2025-09-22 17:02:07 +0100

    fixed the onboarding layout issue

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -9,7 +9,7 @@ const OnboardingPage = () => {
   }, []);
 
   return (
-    <div>
+    <div className="flex min-h-screen items-center justify-center  p-4">
       <div className="flex flex-col items-center gap-4 text-center rounded-lg border border-dashed shadow-sm bg-white p-8 max-w-md w-full">
         <Loader2 className="h-10 w-10 animate-spin text-blue-300" />
         <h3 className="text-2xl font-bold tracking-tight text-gray-900">
