f9dcc1460c32298dce71d7d45e45a52a9d2de0e9
mikofranco <ogbechiemicheal@gmail.com>
2026-01-06 09:33:03 +0100

    added force-dynamic to the agencies/1d in admins

--- app/dashboard/admin/agencies/[id]/page.tsx
+++ app/dashboard/admin/agencies/[id]/page.tsx
@@ -1,6 +1,9 @@
 import AgencyProfile from '@/parts/superAdmin/agency/ageny-info'
 import React from 'react'
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 const AgencyDetails = () => {
   return (
     <div><AgencyProfile/></div>
