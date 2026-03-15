5dc6351853b8c6ffee0f7d35c81034263a248499
mikofranco <ogbechiemicheal@gmail.com>
2026-03-15 21:33:50 +0100

    pulled branches into micheal branch

--- components/closing-remarks.tsx
+++ components/closing-remarks.tsx
@@ -210,6 +210,7 @@ export function ClosingRemarksModal({
                 Mark as Completed
               </Button>
             </DialogFooter>
+            
           </>
         ) : (
           // Success view
